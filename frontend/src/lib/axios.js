10a7b03a0b07887ba346fb5ed687a2181f8cf98e
mohitcusat <mohitchauhan7079@gmil.com>
2026-06-05 08:23:03 +0530

    Fix production API URL

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5001/api"
+      : "https://real-time-chat-application-9sd2.onrender.com/api",
   withCredentials: true,
 });
