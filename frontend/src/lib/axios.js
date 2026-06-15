86386f5d95c4e87f3fcca92fc0c4414a1e5e039d
mohitcusat <mohitchauhan7079@gmil.com>
2026-06-15 17:40:34 +0530

    Updated backend URL

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:5001/api"
-      : "https://real-time-chat-application-9sd2.onrender.com/api",
+      : "https://real-time-chat-application-1-9sd2.onrender.com/api",
   withCredentials: true,
 });
