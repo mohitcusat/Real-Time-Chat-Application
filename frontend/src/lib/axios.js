29826de67e1363e51da250f737dde0c798475cc1
mohitcusat <mohitchauhan7079@gmil.com>
2026-06-15 19:34:18 +0530

    Fix URL

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:5001/api"
-      : "https://real-time-chat-application-1-9sd2.onrender.com/api",
+      : import.meta.env.VITE_API_URL + "/api",
   withCredentials: true,
 });
